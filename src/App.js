62df28849cdf2fe2668d01b31906fc46301d4c90
Danniels Castillo <danniels.magana@gmail.com>
2021-09-25 16:02:30 -0600

    Install and Configure TailwindCSS

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const App = () => {
     return (
-        <div>
-            <h1>Hello World!!</h1>
+        <div className="container p-4 bg-gray-200">
+            <h1 className="bg-indigo-200">Hello World!!</h1>
         </div>
     )
 }

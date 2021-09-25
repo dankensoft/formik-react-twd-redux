cbbbbf88bc514d95ae9241611e8551b91ebbb9fb
Danniels Castillo <danniels.magana@gmail.com>
2021-09-25 16:28:13 -0600

    Form Component: Design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Form } from './Form'
 
 export const App = () => {
     return (
-        <div className="container p-4 bg-gray-200">
-            <h1 className="bg-indigo-200">Hello World!!</h1>
+        <div className="container p-4">
+            <Form/>
         </div>
     )
 }
